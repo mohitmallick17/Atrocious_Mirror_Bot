6f181649261850f4c90fbfccc8affe360d799c67
AtrociousMirrorBot <101085500+AtrociousMirrorBot@users.noreply.github.com>
2022-11-24 09:55:10 +0600

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -1,14 +1,16 @@
 from time import sleep, time
 from telegram import InlineKeyboardMarkup
+from telegram.message import Message
 from telegram.error import RetryAfter
 from pyrogram.errors import FloodWait
 from os import remove
 
-from bot import LOGGER, status_reply_dict, status_reply_dict_lock, Interval, bot, rss_session
+from bot import AUTO_DELETE_MESSAGE_DURATION, LOGGER, status_reply_dict, status_reply_dict_lock, \
+                Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL, RSS_CHAT_ID, bot, rss_session, AUTO_DELETE_UPLOAD_MESSAGE_DURATION
 from bot.helper.ext_utils.bot_utils import get_readable_message, setInterval
 
 
-def sendMessage(text, bot, message):
+def sendMessage(text: str, bot, message: Message):
     try:
         return bot.sendMessage(message.chat_id,
                             reply_to_message_id=message.message_id,
@@ -21,7 +23,7 @@ def sendMessage(text, bot, message):
         LOGGER.error(str(e))
         return
 
-def sendMarkup(text, bot, message, reply_markup: InlineKeyboardMarkup):
+def sendMarkup(text: str, bot, message: Message, reply_markup: InlineKeyboardMarkup):
     try:
         return bot.sendMessage(message.chat_id,
                             reply_to_message_id=message.message_id,
@@ -35,7 +37,7 @@ def sendMarkup(text, bot, message, reply_markup: InlineKeyboardMarkup):
         LOGGER.error(str(e))
         return
 
-def editMessage(text, message, reply_markup=None):
+def editMessage(text: str, message: Message, reply_markup=None):
     try:
         bot.editMessageText(text=text, message_id=message.message_id,
                               chat_id=message.chat.id,reply_markup=reply_markup,
@@ -71,19 +73,20 @@ def sendRss(text: str, bot):
             LOGGER.error(str(e))
             return
 
-def deleteMessage(bot, message):
+def deleteMessage(bot, message: Message):
     try:
-        bot.deleteMessage(chat_id=message.chat.id, message_id=message.message_id)
-    except:
-        pass
+        bot.deleteMessage(chat_id=message.chat.id,
+                           message_id=message.message_id)
+    except Exception as e:
+        LOGGER.error(str(e))
 
-def sendLogFile(bot, message):
+def sendLogFile(bot, message: Message):
     with open('log.txt', 'rb') as f:
         bot.sendDocument(document=f, filename=f.name,
                           reply_to_message_id=message.message_id,
                           chat_id=message.chat_id)
 
-def sendFile(bot, message, name, caption=""):
+def sendFile(bot, message: Message, name: str, caption=""):
     try:
         with open(name, 'rb') as f:
             bot.sendDocument(document=f, filename=f.name, reply_to_message_id=message.message_id,
@@ -98,12 +101,26 @@ def sendFile(bot, message, name, caption=""):
         LOGGER.error(str(e))
         return
 
-def auto_delete_message(bot, cmd_message, bot_message):
+def auto_delete_message(bot, cmd_message: Message, bot_message: Message):
     if AUTO_DELETE_MESSAGE_DURATION != -1:
         sleep(AUTO_DELETE_MESSAGE_DURATION)
-        deleteMessage(bot, cmd_message)
-        deleteMessage(bot, bot_message)
-
+        try:
+            # Skip if None is passed meaning we don't want to delete bot xor cmd message
+            deleteMessage(bot, cmd_message)
+            deleteMessage(bot, bot_message)
+        except AttributeError:
+            pass
+def auto_delete_upload_message(bot, cmd_message: Message, bot_message: Message):
+    if cmd_message.chat.type == 'private':
+        pass
+    elif AUTO_DELETE_UPLOAD_MESSAGE_DURATION != -1:
+        sleep(AUTO_DELETE_UPLOAD_MESSAGE_DURATION)
+        try:
+            # Skip if None is passed meaning we don't want to delete bot or cmd message
+            deleteMessage(bot, cmd_message)
+            deleteMessage(bot, bot_message)
+        except AttributeError:
+            pass
 def delete_all_messages():
     with status_reply_dict_lock:
         for data in list(status_reply_dict.values()):
@@ -115,7 +132,7 @@ def delete_all_messages():
 
 def update_all_messages(force=False):
     with status_reply_dict_lock:
-        if not status_reply_dict or not Interval or (not force and time() - list(status_reply_dict.values())[0][1] < 3):
+        if not force and (not status_reply_dict or not Interval or time() - list(status_reply_dict.values())[0][1] < 3):
             return
         for chat_id in status_reply_dict:
             status_reply_dict[chat_id][1] = time()
@@ -151,4 +168,4 @@ def sendStatusMessage(msg, bot):
             message = sendMarkup(progress, bot, msg, buttons)
         status_reply_dict[msg.chat.id] = [message, time()]
         if not Interval:
-            Interval.append(setInterval(STATUS_UPDATE_INTERVAL, update_all_messages))
+            Interval.append(setInterval(DOWNLOAD_STATUS_UPDATE_INTERVAL, update_all_messages))
